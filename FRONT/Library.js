61d1c11f1a814f6486f5038c761ca125679b261e
Brian Fischer <starfox139@yahoo.com>
2017-11-30 20:07:01 -0500

    Update Library.js

--- FRONT/Library.js
+++ FRONT/Library.js
@@ -1,1 +1,36 @@
+var Library = function(){
+	this.populateFromDB = function(){
+		$.ajax({
+			//
+			success : function(){
+				//
+			}
+		});
+	}
+	
+	// first checks the link to see if it's valid.
+	this.addToLibrary = function(link){ NOTIMPLEMENTED() }
+}
 
+var Song = function(){
+	this.title = "title not set";
+	this.artist = "artist not set";
+	this.album = "album not set";
+	this.genre = "genre not set";
+	this.tags = [];
+	
+	this.link = "link not set";
+	var fromYT = undefined;
+	
+	// SC uses milliseconds
+	// YT uses seconds
+	var startTime = 0;
+	var endTime = 0;
+	
+	this.getStartTimeSeconds = function(){
+		return fromYT ? startTime : startTime * 1000;
+	}
+	this.setStartTimeSeconds = function(seconds){
+		startTime = fromYT ? seconds : seconds * 1000;
+	}
+}
