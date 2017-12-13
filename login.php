dc30232a9e04df938b714d22db9ee7374dbe835e
rjp1215 <32282852+rjp1215@users.noreply.github.com>
2017-12-13 15:02:24 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,42 @@
-<php
+<?php
+
+$name = 'mckellydb';
+$user = 'mckelly';
+$pass = 'password';
+$host = 'classroom.cs.unc.edu';
+$correct = 0;
+
+//$login = $_POST['login'];
+//$password = $_POST['pass'];
+//$login = "user";
+//$password = "pass";
+
+// Begin connection
+/*$connect = mysqli_connect($host, $user, $pass)
+or die("Unable to Connect to '$host'");
+
+
+mysqli_select_db($connect, 'mckellydb');
+
+
+
+// create string for sql query
+//$pass_test = "SELECT Password FROM User u WHERE u.Login='$login' limit 1";
+$pass_res = mysqli_query($connect, "SELECT Password FROM User u WHERE u.Login='".$login."' limit 1");
+$pass_temp = mysqli_fetch_object($pass_res);
+//var_dump($pass_temp);
+
+$pass_val = $pass_temp->Password;
+
+
+
+if($pass_val == $password){$correct = 1;}
+
+//$data = {password: $pass_val, correct: $correct};*/
+
+
+
+
+
+
+?>
