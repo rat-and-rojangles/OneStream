4bddf71ff6adc0aa5aee78e145a9195f28a525a7
Brian Fischer <starfox139@yahoo.com>
2017-12-13 23:20:33 -0500

    test and components

--- JS/Player/SampleComponents.js
+++ JS/Player/SampleComponents.js
@@ -1,8 +1,18 @@
-var generateSongEntry = function (data) {
-	var html = '<div class="song-entry"><li>';
-	html += data.title + '</li><ul>'
-	html += '<li>' + data.artist + ' | ' + data.album + '</li>'
-	html += '</ul></div>';
+var generateSongEntry = function (song) {
+	var html = `<div class="song-view container-fluid">
+	<div class="row">
+		<div class="col-sm-6">
+			<h5>`+ 'title' + `</h5>
+			<br>
+			<div> <a class="artist-link" href="">`+ song.artist + `</a> | <a class="album-link" href="">` + song.album + `</a></div>
+		</div >
+	<div class="col-sm-6 text-right">
+		<button class="btn queue-next-button">Queue Next</button>
+		<button class="btn queue-end-button">Queue Last</button>
+		<button class="btn edit-button">EDIT</button>
+	</div>
+	</div>
+</div > `
 	return html;
 }
 
