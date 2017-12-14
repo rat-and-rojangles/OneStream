34f5124ab7a808957a332eea038852e1d7baeba8
Brian Fischer <starfox139@yahoo.com>
2017-12-13 22:42:33 -0500

    song.js

--- login.js
+++ login.js
@@ -1,37 +1,37 @@
-$(function() {
-    $('body').removeClass('fade-out');
+$(function () {
+	$('body').removeClass('fade-out');
 });
 
 
-setTimeout(function() {
-  $('#banner').remove();
-  $('#screen-1').css({ 'display' : 'inline' });
+setTimeout(function () {
+	$('#banner').remove();
+	$('#screen-1').css({ 'display': 'inline' });
 }, 3000);
 
-$(document).ready(function() {
-    $("#login-form").on('submit', function (e) {
+$(document).ready(function () {
+	$("#login-form").on('submit', function (e) {
 
-        e.preventDefault();
+		e.preventDefault();
 
-        var email = $("#un").val();
-        var pass = $("#pwd").val();
+		var email = $("#un").val();
+		var pass = $("#pwd").val();
 
-        var url = "login.php";
-        var data = {email: email, pass: pass};
+		var url = "login.php";
+		var data = { email: email, pass: pass };
 
-        $.ajax({
-            type: "GET",
-            url: url,
-            data: data,
-            dataType: 'JSON',
-            success: function (result) {
-                console.log(result);
-                $("#screen-1").remove();
-                var user_id = result[0].ID;
-            },
-            error: function (result) {
-                console.log(result);
-            }
-        });
-    });
+		$.ajax({
+			type: "GET",
+			url: url,
+			data: data,
+			dataType: 'JSON',
+			success: function (result) {
+				console.log(result);
+				$("#screen-1").remove();
+				var user_id = result[0].ID;
+			},
+			error: function (result) {
+				console.log(result);
+			}
+		});
+	});
 });
