4ad0501d22375ff526a16f11ff115840484f04a7
Brian Fischer <starfox139@yahoo.com>
2017-12-13 21:38:04 -0500

    minor js changes

--- JS/Driver.js
+++ JS/Driver.js
@@ -1,13 +1,13 @@
 var player;
 var controls;
-var library;	// ideally this should be set up when the user logs in.
-var currentSongs = [];
+var library;	// should construct when the user logs in.
+var currentSelectedSongs = [];
 $(document).ready(function () {
 	controls = new PlayControls($('#pausePlay'), $('#forward'), $('#backward'), $('#slider'));
 	player = new Player();
 	player.registerCallback(Player.events.READY, function () { controls.setEnabled(true) });
 
-	library = new Library();
+	library = new Library(1);
 
 	$('#submitBox').on('click', function () {
 		alert(library.filterByArtist($('#inputBox').val()).length);
