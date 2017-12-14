4f72f00672c7b2660f415462e40d679de4f76f0d
rjp1215 <32282852+rjp1215@users.noreply.github.com>
2017-12-14 00:24:02 -0500

    Added playplace for library selector

--- login.js
+++ login.js
@@ -28,6 +28,9 @@ $(document).ready(function () {
 				console.log(result);
 				$("#screen-1").remove();
 				var user_id = result[0].ID;
+				var library = $("<div class='song-view container-fluid' style='margin-top:20px;'><div class='row'><div class='col-sm-8'><br /><h1>Library</h1><br /></div><div style='flex-direction:row;'><button style='margin-right:10px;' class='btn addbtns' id='play-button' onclick='alert(1)'>Play</button><button class='btn addbtns' id='shuffle-button'>Shuffle</button><button class='btn addbtns top-buffer' id='add-song'>Add Song</button></div></div></div>");
+				$("#library-choice").on('click', function (e) {$("#play-place").empty(); $("#play-place").append(library);});
+
 			},
 			error: function (result) {
 				console.log(result);
