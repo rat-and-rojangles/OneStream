a7300d04cd3b91247bd4dc664a85c36ede937814
mckelly96 <mikethebike96@gmail.com>
2017-12-14 17:25:37 -0500

    add song to playlist script

--- PHP+Forms/AddSongToPlaylist.php
+++ PHP+Forms/AddSongToPlaylist.php
@@ -17,9 +17,9 @@ or die("Unable to Connect to '$dbhost'");
 
 // troubleshoot if connect worked
 // NOTE: this only works by running the php file by itself and not through HTML
-if($connect){
-    echo "Connected to host<br>";
-}
+//if($connect){
+//    echo "Connected to host<br>";
+//}
 
 // select db on server
 mysqli_select_db($connect, 'mckellydb')
@@ -28,11 +28,11 @@ or die("Could not open the db '$dbname'");
 // create string for sql query
 $sql = "INSERT INTO SongPlaylistJunction(Song_ID, Playlist_ID, User_ID) VALUES ($song_id, $playlist_id, $user_id)";
 
-if (mysqli_query($connect, $sql)) {
-    echo "HTTP/1.0 200 Good Request";
-} else {
-    echo "HTTP/1.0 400 Bad Request";
-}
+//if (mysqli_query($connect, $sql)) {
+//    echo "HTTP/1.0 200 Good Request";
+//} else {
+//    echo "HTTP/1.0 400 Bad Request";
+//}
 
 mysqli_close($connect);
 ?>
